567d89f5ff0b740534ababda1fa72db5a0e5585c
Wyatt Houston <h.wy4tt@gmail.com>
2021-08-14 17:06:52 -0400

    updated portfolio

---
+++ components/Progress.js
@@ -0,0 +1,8 @@
+document.write(`
+<div class="container">
+<ul class="progressbar">
+    <li class="active">Wireframing</li>
+    <li class="active">Language selection</li>
+    <li>Coding it up</li>
+    <li>Publishing</li>
+</ul>`);
