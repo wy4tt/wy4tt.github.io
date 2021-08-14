eebca18d8709c5dd63dfd75cf515b8edb903b108
Wyatt Houston <h.wy4tt@gmail.com>
2021-08-14 17:20:21 -0400

    added missing closing div tag

--- components/Progress.js
+++ components/Progress.js
@@ -1,8 +1,9 @@
 document.write(`
 <div class="container">
-<ul class="progressbar">
-    <li class="active">Wireframing</li>
-    <li class="active">Language selection</li>
-    <li>Coding it up</li>
-    <li>Publishing</li>
-</ul>`);
+    <ul class="progressbar">
+        <li class="active">Wireframing</li>
+        <li class="active">Language selection</li>
+        <li>Coding it up</li>
+        <li>Publishing</li>
+    </ul>
+</div>`);
