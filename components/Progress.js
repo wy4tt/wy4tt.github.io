1811036c9112311af8e0063c38489f9fbacafcd4
Wyatt Houston <h.wy4tt@gmail.com>
2022-10-06 16:16:35 -0400

    Update Progress.js

--- components/Progress.js
+++ components/Progress.js
@@ -6,8 +6,7 @@ document.write(`
 
     <ul class="progressbar">
         <li class="active">Design</li>
-        <li class="active">Language selection</li>
-        <li>Code it up</li>
+        <li class="active">Code it up</li>
         <li>Publish</li>
     </ul>
 </div>`);
